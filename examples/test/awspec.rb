bb6562922679e6c5863bb3f095cb70394398ff5b
Ivan Echegaray <ivan.echegaray@outlook.com>
2021-04-09 22:53:03 -0500

    fix output

--- examples/test/awspec.rb
+++ examples/test/awspec.rb
@@ -1,6 +1,6 @@
 require 'awspec'
 
-service_name = `cd examples/test/ && terraform output id`.strip
+service_name = `cd examples/test/ && terraform output id`.strip.delete('"')
 
 describe cloudfront_distribution(service_name.to_s) do
     it { should exist }
